a32ec425ba94565d0991c268e511791d747a9e6c
Maxime Bizeray <ext.maxime.bizeray@lajavaness.com>
2026-04-30 23:51:34 +0200

    fix: fix attackSpeed par défaut pour les Monster

--- core/src/main/kotlin/fr/thebrisingers/slotmachinegame/data/caracter/Monster.kt
+++ core/src/main/kotlin/fr/thebrisingers/slotmachinegame/data/caracter/Monster.kt
@@ -4,5 +4,5 @@ data class Monster(
     val id: Int,
     val category: Category,
     val faction: Faction,
-    val attackSpeed: Int,
+    val attackSpeed: Int = category.attackSpeed + faction.attackSpeedModifier,
 )
