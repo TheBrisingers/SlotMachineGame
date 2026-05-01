6333cd5d2cf5f6c78ad87bbc9a29bce2dc1376e1
tim <timothee.jouffrieau@gmail.com>
2026-05-01 11:02:46 +0200

    feat: Detecter simple et hold press

--- core/src/main/kotlin/fr/thebrisingers/slotmachinegame/SlotMachineGame.kt
+++ core/src/main/kotlin/fr/thebrisingers/slotmachinegame/SlotMachineGame.kt
@@ -20,9 +20,12 @@ class SlotMachineGame : KtxGame<KtxScreen>() {
     }
 }
 
-class FirstScreen : KtxScreen {
-    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
-    private val batch = SpriteBatch()
+class FirstScreen : KtxScreen, InputAdapter() {
+    private val stage = Stage(FitViewport(1280f, 720f))
+
+    override fun show() {
+        Gdx.input.inputProcessor = this
+    }
 
     override fun render(delta: Float) {
         clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
@@ -34,5 +37,39 @@ class FirstScreen : KtxScreen {
     override fun dispose() {
         image.disposeSafely()
         batch.disposeSafely()
+    }
+
+
+    private var spaceHoldStartTime = 0L
+    private val holdThresholdMs = 500L // seuil hold = 500ms
+
+    override fun keyDown(keycode: Int): Boolean {
+        if (keycode == Keys.SPACE) {
+            spaceHoldStartTime = System.currentTimeMillis()
+        }
+        return true
+    }
+
+    override fun keyUp(keycode: Int): Boolean {
+        if (keycode == Keys.SPACE) {
+            val duration = System.currentTimeMillis() - spaceHoldStartTime
+            if (duration < holdThresholdMs) {
+                changeFocus()
+            } else {
+                applyAction()
+            }
+        }
+        return true
+    }
+
+    val isPress: Boolean
+        get() = Gdx.input.isKeyPressed(Keys.SPACE)
+
+    private fun changeFocus() {
+
+    }
+
+    private fun applyAction() {
+
     }
 }
